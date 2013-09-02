97257a3b5bf60ce4072ed649f86f865237b9346c
Felipe Oliveira <felipe.oliveira@abril.com.br>
2013-09-02 18:30:19 -0300

    Debugging

--- lib/easy_config/path_resolver.rb
+++ lib/easy_config/path_resolver.rb
@@ -4,13 +4,18 @@ module EasyConfig::PathResolver
   end
 
   def self.config_path
+    puts 'config_path'
     if defined?(@config_path) and @config_path
+      puts 'config_path => 1'
       File.join(@config_path, '*.yml')
     elsif defined?(Rails)
+      puts 'config_path => 2'
       File.join(Rails.root, 'config', '*.yml')
     elsif defined?(Sinatra)
+      puts 'config_path => 3'
       File.join(Sinatra::Application.root, 'config', '*.yml')
     else
+      puts 'config_path => 4'
       raise EasyConfig::UnknownConfigPath.new
     end
   end
