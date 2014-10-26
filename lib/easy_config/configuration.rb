c67c211d0115c7e8462571d2d020d7c583b27bb9
Felipe Oliveira <felipecvo@gmail.com>
2014-10-26 02:17:26 -0200

    Refactor to define methods dynamically instead of method missing.

--- lib/easy_config/configuration.rb
+++ lib/easy_config/configuration.rb
@@ -1,11 +1,25 @@
-require 'methodize'
+require 'ostruct'
 
 class EasyConfig::Configuration
   def initialize(config)
-    @config = config.extend(Methodize)
+    create_methods(config)
+  end
+
+  def create_methods(config)
+    config.each do |key, value|
+      value = OpenStruct.new(value) if value.is_a?(Hash)
+      define_singleton_method(key.to_sym) { value }
+    end
   end
 
   def method_missing(name, *args)
-    @config.send(name, *args) rescue nil
+    nil
+  end
+
+  if RUBY_VERSION < "1.9"
+    def define_singleton_method(name, &block)
+      singleton = class << self; self end
+      singleton.send(:define_method, name, &block)
+    end
   end
 end
